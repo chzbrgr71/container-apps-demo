58213510215f82b2c9a42ef067932d0df743a555
Brian Redmond <brianisrunning@gmail.com>
2022-02-01 15:50:55 -0700

    started on order service

--- order-service/routes/base.js
+++ order-service/routes/base.js
@@ -3,6 +3,20 @@ module.exports = function (fastify, opts, next) {
     fastify.get('/', (req, reply) => {
         reply.send({ "orderService": "running", "status": "ok", "version": "0.10" })
     })
+
+    const options = {
+        schema: {
+            querystring: {
+                id: { type: 'string' }
+            }
+        }
+      }
+    
+    fastify.get('/orderbyid', options, async (request, reply) => {
+        var orderId = request.query.id
+    
+        return ({ "id": orderId, "order": "this is an order" })
+    })
   
     next()
 }
